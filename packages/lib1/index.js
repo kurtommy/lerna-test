9bab0b72c5697669d54ca7bc05655c9eeeb482e7
Tommaso Rossi <trossi@cartrawler..com>
2017-11-28 13:24:04 +0100

    asd

--- packages/lib1/index.js
+++ packages/lib1/index.js
@@ -1,5 +1,6 @@
 f2 = require("tom-lib2");
 module.exports = function(a) {
   console.log("lib 1 mod 6", a);
-  console.log("this is f2 called inside f1", f2());
+  console.log("next line is f2 called inside f1");
+  f2();
 };
--- packages/lib2/index.js
+++ packages/lib2/index.js
@@ -1,3 +1,3 @@
 module.exports = function(a) {
-  console.log("lib 2 mod 10", a);
+  console.log("lib 2 mod 10 xx", a);
 };
