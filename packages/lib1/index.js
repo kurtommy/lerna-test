da387b6f9f8e727a4c43594c097a07184552a69b
Tommaso Rossi <trossi@cartrawler..com>
2017-11-28 11:40:58 +0100

    3

--- packages/lib1/index.js
+++ packages/lib1/index.js
@@ -1,3 +1,3 @@
 module.exports = function(a) {
-  console.log("lib 1 mod 2", a);
+  console.log("lib 1 mod 3", a);
 };
--- packages/lib2/index.js
+++ packages/lib2/index.js
@@ -1,3 +1,3 @@
 module.exports = function(a) {
-  console.log("lib 2", a);
+  console.log("lib 2 mode 1", a);
 };
